c64fc206637de7deea192de2624b4a0b2bbf61dd
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-06-25 21:33:48 -0500

    feat: Add Gotham font files and import statement

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,87 @@
+import localFont from 'next/font/local'
+
+export const Gotham = localFont({
+  src: [
+    {
+      path: 'fonts/Gotham-Black.otf',
+      style: 'normal',
+      weight: '450',
+    },
+    {
+      path: 'fonts/Gotham-BlackItalic.otf',
+      style: 'italic',
+      weight: '450',
+    },
+    {
+      path: 'fonts/Gotham-Bold.otf',
+      style: 'normal',
+      weight: '400',
+    },
+    {
+      path: 'fonts/Gotham-BoldItalic.otf',
+      style: 'italic',
+      weight: '400',
+    },
+    {
+      path: 'fonts/Gotham-Book.otf',
+      style: 'normal',
+      weight: '325',
+    },
+    {
+      path: 'fonts/Gotham-BookItalic.otf',
+      style: 'italic',
+      weight: '325',
+    },
+    {
+      path: 'fonts/Gotham-ExtraLight.otf',
+      style: 'normal',
+      weight: '200',
+    },
+    {
+      path: 'fonts/Gotham-ExtraLightItalic.otf',
+      style: 'italic',
+      weight: '200',
+    },
+    {
+      path: 'fonts/Gotham-Light.otf',
+      style: 'normal',
+      weight: '300',
+    },
+    {
+      path: 'fonts/Gotham-LightItalic.otf',
+      style: 'italic',
+      weight: '300',
+    },
+    {
+      path: 'fonts/Gotham-Medium.otf',
+      style: 'normal',
+      weight: '350',
+    },
+    {
+      path: 'fonts/Gotham-MediumItalic.otf',
+      style: 'italic',
+      weight: '350',
+    },
+    {
+      path: 'fonts/Gotham-Thin.otf',
+      style: 'normal',
+      weight: '100',
+    },
+    {
+      path: 'fonts/Gotham-ThinItalic.otf',
+      style: 'italic',
+      weight: '100',
+    },
+    {
+      path: 'fonts/Gotham-Ultra.otf',
+      style: 'normal',
+      weight: '900',
+    },
+    {
+      path: 'fonts/Gotham-UltraItalic.otf',
+      style: 'italic',
+      weight: '900',
+    },
+  ],
+  variable: '--font-Gotham',
+})
