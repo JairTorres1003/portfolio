45a2d8bc891f136682f744bf15d63553076f84ce
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-06-25 21:38:27 -0500

    feat: Add lazy loading for images in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import MetaData from '@/utils/MetaData'
+
 import { Gotham } from './ui/fonts'
+
+import '@/config/index'
+
+import './ui/globals.css'
+
+export const metadata = MetaData({
+  title: "Home | Jair Torres' Portfolio",
+})
 
 export default function RootLayout({
   children,
